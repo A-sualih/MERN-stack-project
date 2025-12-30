4d1b0ab44cbcb26a13c8357a5dd5d7795bc072aa
Ahmed Sualih <sualihahmed26@gmail.com>
2025-12-30 10:07:35 +0300

    adding Frontend

---
+++ TaskManager/MERN-Stack/FRONTEND/src/places/components/PlaceItem.jsx
@@ -0,0 +1,100 @@
+import React, { useContext, useState } from "react";
+import "./PlaceItem.css";
+import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElement/Card";
+import Modal from "../../shared/components/UIElement/Modal";
+import Map from "../../shared/components/UIElement/Map";
+import { AuthContext } from "../../shared/context/auth-context";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElement/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElement/LoadingSpinner";
+const PlaceItem = (props) => {
+  const auth = useContext(AuthContext);
+  const [showMap, setShowMap] = useState(false);
+  const [showConfirmModal, setShowconfirmModal] = useState(false);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const openMapHandler = () => {
+    setShowMap(true);
+  };
+  const closeMapHandler = () => {
+    setShowMap(false);
+  };
+  const showDeleteWarningHandler = () => {
+    setShowconfirmModal(true);
+  };
+  const cancelDeleteHandler = () => {
+    setShowconfirmModal(false);
+  };
+  const confirmDeleteHandler = async () => {
+    setShowconfirmModal(false);
+    try {
+      await sendRequest(`http://localhost:5001/api/places/${props.id}`, "DELETE");
+      props.onDelete(props.id)
+    } catch (error) { }
+
+    console.log("DELETING....");
+  };
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <Map center={props.coordinates} zoom={16} />
+        </div>
+      </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="place-item__modal-actions"
+        footer={
+          <React.Fragment>
+            <Button onClick={cancelDeleteHandler} inverse>
+              {" "}
+              CANCEl
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              {" "}
+              DELETE
+            </Button>
+          </React.Fragment>
+        }
+      ></Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          {isLoading && <LoadingSpinner asOverlay />}
+          <div className="place-item__image">
+            <img src={`http://localhost:5001/${props.image}`} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button invers onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
+            {auth.userId === props.creatorId && (
+              <Button to={`/places/${props.id}`}>EDIT</Button>
+            )}
+            {auth.userId === props.creatorId && (
+              <Button onClick={showDeleteWarningHandler} danger>
+                DELETE
+              </Button>
+            )}
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
+  );
+};
+
+export default PlaceItem;
---
+++ TaskManager/MERN-Stack/FRONTEND/src/user/pages/Auth.jsx
@@ -0,0 +1,174 @@
+import React, { useContext, useState } from "react";
+import Card from "../../shared/components/UIElement/Card";
+import Input from "../../shared/components/FormElements/Input";
+import ImageUploade from "../../shared/components/FormElements/ImageUploade";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import "./Auth.css";
+import { useForm } from "../../shared/hooks/form-hook";
+import Button from "../../shared/components/FormElements/Button";
+import { AuthContext } from "../../shared/context/auth-context";
+import ErrorModal from "../../shared/components/UIElement/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElement/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+const Auth = () => {
+  const auth = useContext(AuthContext);
+  const [isLogin, setIsLogin] = useState(true);
+  // const [isLoading,setIsLoading]=useState(false);
+  // const [error,setError]=useState(false)
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [formState, inputHandler, setFromData] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+  const authSubmitHandler = async (event) => {
+    event.preventDefault();
+    console.log(formState.inputs)
+    if (isLogin) {
+      // setIsLoading(true)
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:5001/api/users/login",
+          "POST",
+          JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+          {
+            "Content-Type": "application/json",
+          }
+        );
+
+        console.log(responseData);
+
+        auth.login(responseData.user.id);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        const formData=new FormData();
+        formData.append("email",formState.inputs.email.value);
+        formData.append("name",formState.inputs.name.value);
+        formData.append("password",formState.inputs.password.value);
+        formData.append('image',formState.inputs.image.value)
+        const responseData = await sendRequest(
+          "http://localhost:5001/api/users/signup",
+          "POST",
+        formData,
+          // JSON.stringify({
+          //   name: formState.inputs.name.value,
+          //   email: formState.inputs.email.value,
+          //   password: formState.inputs.password.value,
+          // }),
+          // {
+          //   "Content-Type": "application/json",
+          // }
+        );
+
+        auth.login(responseData.user.id);
+        console.log(responseData);
+      } catch (error) {
+        console.log(error);
+        // setIsLoading(false);
+        // setError(error.message || "Something went wrong , please try again")
+        // console.log(error.message || "Signup failed.");
+      }
+    }
+  };
+  const switchModeHandler = () => {
+    if (!isLogin) {
+      setFromData(
+        {
+          ...formState.inputs,
+          name: undefined,
+          image:undefined
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFromData(
+        {
+          ...formState.inputs,
+          name: {
+            value: "",
+            isValid: false,
+            image:{
+          value:null,
+          isValid:false
+            }
+          },
+        },
+        false
+      );
+    }
+    setIsLogin((prevMode) => !prevMode);
+  };
+  // const errorHandler=()=>{
+  //   clearError()
+  // }
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLogin && (
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Your Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="please enter a name"
+              onInput={inputHandler}
+            />
+          )}
+
+          {!isLogin && <ImageUploade center id="image" onInput={inputHandler}/>}
+          <Input
+            element="input"
+            id="email"
+            type="email"
+            label="E-mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email address"
+            onInput={inputHandler}
+          />
+          <Input
+            element="input"
+            id="password"
+            type="password"
+            label="password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid password "
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLogin ? "LOGIN" : "SIGNUP"}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          SWITCH TO {isLogin ? "SIGNUP" : "LOGIN"}
+        </Button>
+      </Card>
+    </React.Fragment>
+  );
+};
+
+export default Auth;
